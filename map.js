7d19fc66d0377ba740261fbc875cc981f3a4ec7a
Suzanne Blake <suzanne.blake22@gmail.com>
2022-07-21 19:54:25 +0000

    map method

--- map.js
+++ map.js
@@ -5,16 +5,24 @@
  */
 
 // Using a for loop
-
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+  results.push(nums*2);
+}
 
 // Using map()
+const multByTwo = function (num) {
+  return num * 2;
+}
 
+const mapResults = nums.map(multByTwo);
 
 // Simplified w/ map()
-
+const simplified = nums.map(function (num) { return num * 2;});
 
 // Simplfied w/ map() + arrow function
-
+const simplified2 = nums.map(num => num*2)
 
 // With objects:
 const students = [
@@ -37,3 +45,5 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map(student => [student.name, student.id]);
